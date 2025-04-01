09b875ae0eecad903b75f95cf83c46e6655bd6bd
hugo-mar <hugo.alberto.dias@gmail.com>
2025-04-01 10:36:52 +0100

    Remove submodule and add minilibx-linux as a normal folder

--- minilibx-linux/mlx_destroy_display.c
+++ minilibx-linux/mlx_destroy_display.c
@@ -1,23 +1,18 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   init.c                                             :+:      :+:    :+:   */
+/*   mlx_destroy_display.c                              :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: hugo-mar <hugo-mar@student.42.fr>          +#+  +:+       +#+        */
+/*   By: mg <mg@student.42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/03/31 17:06:41 by hugo-mar          #+#    #+#             */
-/*   Updated: 2025/04/01 13:11:10 by hugo-mar         ###   ########.fr       */
+/*   Created: 2020/10/03 18:56:35 by mg                #+#    #+#             */
+/*   Updated: 2020/10/04 01:55:35 by mg               ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "cub3d.h"
+#include "mlx_int.h"
 
-/*
-Provides a static pointer to the single global game structure.
-*/
-t_game *get_game(void)
+int	mlx_destroy_display(t_xvar *xvar)
 {
-	static	t_game game;
-
-	return(&game);
+	XCloseDisplay(xvar->display);
 }
